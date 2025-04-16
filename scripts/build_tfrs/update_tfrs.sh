6c05c7b9fbe8f1d3eec2356b8e7c575c99e1b106
Kuriger <mk7193@st1lntloadtes01.corp.pvt>
2025-04-16 17:25:25 -0500

    added

--- scripts/build_tfrs/update_tfrs.sh
+++ scripts/build_tfrs/update_tfrs.sh
@@ -7,13 +7,18 @@ export DATE=$(date "+%y%m%d_%H%M%S")
 #eval $(ssh-agent -s)
 #ssh-add ~/.ssh/id_rsa
 
-cd /data/tfrs || exit
+cd /data/build_tfrs || exit
 #ruby faa_get_tfrs -f geojson tfrs.geojson
 #python3 faa_get_tfrs.py tfrs.geojson
 
 # geojson files
-curl 'https://tfr.faa.gov/geoserver/TFR/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TFR%3AV_TFR_LOC&maxFeatures=50&outputFormat=application%2Fjson' > tfrs.geojson
+#curl 'https://tfr.faa.gov/geoserver/TFR/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TFR%3AV_TFR_LOC&maxFeatures=50&outputFormat=application%2Fjson' > /data/MetarMap/scripts/tfrs.geojson
 
-#git add tfrs.geojson
-#git commit -m "auto-commit $DATE"
-#git push origin main
+python3 faa_get_tfrs.py tfrs.geojson
+/bin/cp tfrs.geojson /data/MetarMap/scripts
+
+cd /data/MetarMap/scripts
+
+git add tfrs.geojson
+git commit -m "auto-commit $DATE"
+git push origin main
